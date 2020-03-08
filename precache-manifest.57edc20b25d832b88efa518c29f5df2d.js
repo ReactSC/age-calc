02fcbe6e83f2a831982d55ed0698adfe70904dbe
Sofiqul Islam <schoolwab@gmail.com>
2020-03-08 15:12:33 +0600

    Updates

---
+++ precache-manifest.57edc20b25d832b88efa518c29f5df2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237dfae19578e402b469ffaefe2fb686",
+    "url": "/age-calc/index.html"
+  },
+  {
+    "revision": "c220db05b9d8858f3d93",
+    "url": "/age-calc/static/css/main.712cea89.chunk.css"
+  },
+  {
+    "revision": "ed70b48fd85bae8dfa73",
+    "url": "/age-calc/static/js/2.c5b6f1d1.chunk.js"
+  },
+  {
+    "revision": "bcbd991f5929e15521a5284398f9c597",
+    "url": "/age-calc/static/js/2.c5b6f1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c220db05b9d8858f3d93",
+    "url": "/age-calc/static/js/main.a4be0b26.chunk.js"
+  },
+  {
+    "revision": "1c131db9c253253dc395",
+    "url": "/age-calc/static/js/runtime-main.f9098b3f.js"
+  }
+]);
